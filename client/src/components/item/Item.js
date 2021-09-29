9786d05c72d9fc53fc70a0dd364141c467cd4f40
CK <wongchuankai97@gmail.com>
2021-09-29 13:29:02 +0800

    item focus in shop product for better ux

--- client/src/components/item/Item.js
+++ client/src/components/item/Item.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container, Row, Nav, Card, Pagination, Dropdown } from 'react-bootstrap'
+import { Container, Row, Nav, Card, Pagination, ListGroup } from 'react-bootstrap'
+import { CardActionArea } from '@mui/material';
 import { useHistory } from 'react-router-dom'
 
 function Item({ productID, itemName, price, description, image}) {
@@ -11,13 +12,15 @@ function Item({ productID, itemName, price, description, image}) {
     
     return (
         <Card key={productID} id={productID} style={{ width: '18rem', marginRight:"30px" }} onClick={handleClick}>
-            <Card.Img className="cardImg" src={image}/>
-            <Card.Body>
-            <Card.Title style={{textAlign:"center"}}>{itemName}</Card.Title>
-            <Card.Text style={{textAlign: "center"}}>
-                ${price}
-            </Card.Text>
-            </Card.Body>
+            <CardActionArea>
+                <Card.Img className="cardImg" src={image}/>
+                <Card.Body>
+                <Card.Title style={{textAlign:"center"}}>{itemName}</Card.Title>
+                <Card.Text style={{textAlign: "center"}}>
+                    ${price}
+                </Card.Text>
+                </Card.Body>
+            </CardActionArea>
         </Card>
     )
 }
